87b712a9057b61162c1b3cc8a32d573f4eb3de62
Vedantgad2509 <vedantgad25@gmail.com>
2023-11-23 00:37:15 +0530

    reasearch

--- update/insert_data_research.php
+++ update/insert_data_research.php
@@ -1,19 +1,19 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $doi = $_POST["doi"];
-    $id = $_POST["id"];
-    $roll_no = $_POST["Roll_no"];
-    $title = $_POST["title"];
-    $journal_name = $_POST["journal_name"];
-    $volume = $_POST["volume"];
-    $series = $_POST["series"];
-    $year = $_POST["year"];
-    $domain = $_POST["domain"];
+    $doi = $_POST["DOI"];
+    $id = $_POST["ID"];
+    $roll_no = $_POST["Roll_No"];
+    $title = $_POST["Title"];
+    $journal_name = $_POST["Journal name"];
+    $volume = $_POST["Volume"];
+    $series = $_POST["Series"];
+    $year = $_POST["Year"];
+    $domain = $_POST["Domain"];
     $activity_type = $_POST["activity_type"];
 
     $conn = mysqli_connect("localhost", "root", "", "dmqp_db");
-    $sql = "INSERT INTO research_paper_publication (DOI, ID, Roll_No, Title, `Journal name`, Volume, Series, Year, Domain, activity_type)
-            VALUES ('$doi', '$id', '$roll_no', '$title', '$journal_name', '$volume', '$series', '$year', '$domain', '$activity_type')";
+    $sql = "INSERT INTO `research_paper_publication`(`DOI`, `Title`, `Journal name`, `Volume`, `Series`, `Year`, `Domain`, `activity_type`, `Roll_No`, `ID`)
+            VALUES ('$doi','$title', '$journal_name', '$volume', '$series', '$year', '$domain', '$activity_type','$roll_no','$id')";
     if ($conn->query($sql) === TRUE) {
         echo "New entry added successfully!";
     } else {
